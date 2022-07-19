e110e516539fd9a30c87c41b26156cb47c2fe796
Nathan Kessler <nathant93@gmail.com>
2022-07-18 21:55:03 -0400

    Working scanner implementation

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use std::{
     process,
 };
 
-use unicode_segmentation::{GraphemeIndices, UnicodeSegmentation};
-
 fn main() {
     let mut lox: Lox = Lox::new();
 
@@ -60,9 +58,17 @@ impl Lox {
     fn run_prompt(&mut self) {
         loop {
             print!("> ");
-            std::io::stdout().flush().unwrap();
+            match std::io::stdout().flush() {
+                Ok(_) => (),
+                Err(_) => panic!("Error writing to stdout"),
+            }
             let mut line = String::new();
-            std::io::stdin().read_line(&mut line).unwrap();
+
+            match std::io::stdin().read_line(&mut line) {
+                Ok(_) => (),
+                Err(_) => panic!("idk"),
+            };
+
             let line = line.trim();
             if line.len() == 0 {
                 continue;
@@ -74,11 +80,11 @@ impl Lox {
 
     fn run(&self, source: &str) {
         let mut scanner = Scanner::new(source);
-        scanner.scan_tokens();
+        let tokens = scanner.scan_tokens();
 
-        // for token in tokens {
-        //     println!("{}", token);
-        // }
+        for token in tokens {
+            println!("{}", token);
+        }
     }
 
     pub fn error(line: usize, message: &str) {
@@ -91,84 +97,262 @@ impl Lox {
 }
 
 struct Scanner<'a> {
-    source: GraphemeIndices<'a>,
-    tokens: Vec<Token>,
-    current: String,
+    source: &'a str,
+    tokens: Vec<Token<'a>>,
+    start: usize,
+    current: usize,
     line: usize,
-}
-
-enum ScanStatus {
-    Working,
-    Finished,
 }
 
 impl<'a> Scanner<'a> {
     pub fn new(source: &'a str) -> Scanner {
         Scanner {
-            source: source.grapheme_indices(true),
+            source,
             tokens: Vec::new(),
-            current: String::new(),
+            start: 0,
+            current: 0,
             line: 1,
         }
     }
 
-    pub fn scan_tokens(&mut self) {
-        match self.scan_token() {
-            ScanStatus::Working => (),
-            ScanStatus::Finished => {
-                self.tokens.push(Token::new(
-                    TokenType::Eof,
-                    String::from(""),
-                    None,
-                    self.line,
-                ));
-            }
+    pub fn scan_tokens(&mut self) -> &Vec<Token> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
         }
-    }
 
-    fn scan_token(&mut self) -> ScanStatus {
-        let c = self.advance();
-
-        match c {
-            None => ScanStatus::Finished,
-            Some(s) => {
-                match s.as_str() {
-                    "(" => self.add_token(TokenType::LeftParen),
-                    ")" => self.add_token(TokenType::RightParen),
-                    "{" => self.add_token(TokenType::LeftBrace),
-                    "}" => self.add_token(TokenType::RightBrace),
-                    "," => self.add_token(TokenType::Comma),
-                    "." => self.add_token(TokenType::Dot),
-                    "-" => self.add_token(TokenType::Minus),
-                    "+" => self.add_token(TokenType::Plus),
-                    ";" => self.add_token(TokenType::Semicolon),
-                    "*" => self.add_token(TokenType::Star),
-                    _ => Lox::error(self.line, &format!("Unexpected character: {}", s)),
-                };
-                ScanStatus::Working
-            }
-        }
-    }
-
-    fn advance(&mut self) -> Option<String> {
-        let result = self.source.next();
-        return match result {
-            Some((_, r)) => Some(r.to_string()),
-            None => None,
-        };
-    }
-
-    fn add_token(&mut self, token_type: TokenType) {
         self.tokens.push(Token::new(
-            token_type,
-            self.current.clone(),
+            TokenType::Eof,
+            String::from(""),
             None,
             self.line,
         ));
-        self.current = String::new();
+
+        return &self.tokens;
     }
 
-    fn match_next(&mut self) {}
+    fn scan_token(&mut self) {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '!' => {
+                let token = if self.match_next('=') {
+                    TokenType::BangEqual
+                } else {
+                    TokenType::Bang
+                };
+
+                self.add_token(token);
+            }
+            '=' => {
+                let token = if self.match_next('=') {
+                    TokenType::EqualEqual
+                } else {
+                    TokenType::Equal
+                };
+
+                self.add_token(token);
+            }
+            '<' => {
+                let token = if self.match_next('=') {
+                    TokenType::LessEqual
+                } else {
+                    TokenType::Less
+                };
+
+                self.add_token(token);
+            }
+            '>' => {
+                let token = if self.match_next('=') {
+                    TokenType::GreaterEqual
+                } else {
+                    TokenType::Greater
+                };
+
+                self.add_token(token);
+            }
+            ' ' | '\r' | '\t' => (), // Ignore
+            '\n' => self.line += 1,
+            '"' => self.string(),
+            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => self.number(),
+            _ => {
+                if Scanner::is_alpha(Some(c)) {
+                    self.indentifier();
+                } else {
+                    Lox::error(self.line, &format!("Unexpected character: {}", c))
+                }
+            }
+        };
+    }
+
+    fn advance(&mut self) -> char {
+        let result = self.source.as_bytes()[self.current] as char;
+        self.current += 1;
+        result
+    }
+
+    fn peek(&self) -> Option<char> {
+        if self.is_at_end() {
+            None
+        } else {
+            Some(self.source.as_bytes()[self.current] as char)
+        }
+    }
+
+    fn peek_next(&self) -> Option<char> {
+        if self.current + 1 >= self.source.len() {
+            None
+        } else {
+            Some(self.source.as_bytes()[self.current + 1] as char)
+        }
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        let text = &self.source[self.start..self.current];
+
+        self.tokens
+            .push(Token::new(token_type, text.to_string(), None, self.line));
+    }
+
+    fn add_token_literal(&mut self, token_type: TokenType, literal: Literal<'a>) {
+        let text = &self.source[self.start..self.current];
+
+        self.tokens.push(Token::new(
+            token_type,
+            text.to_string(),
+            Some(literal),
+            self.line,
+        ));
+    }
+
+    fn match_next(&mut self, expected: char) -> bool {
+        if self.is_at_end() || self.source.as_bytes()[self.current] as char != expected {
+            return false;
+        }
+
+        self.current += 1;
+        true
+    }
+
+    fn string(&mut self) {
+        while self.peek() != Some('"') {
+            if self.peek() == Some('\n') {
+                self.line += 1;
+            }
+            if self.is_at_end() {
+                Lox::error(self.line, "Unterminated string.");
+                return;
+            }
+
+            self.advance();
+        }
+
+        // Consume the closing quote
+        self.advance();
+
+        let literal = Literal {
+            literal_type: LiteralType::String,
+            value: LiteralValue {
+                string: &self.source[self.start + 1..self.current - 1],
+            },
+        };
+
+        self.add_token_literal(TokenType::String, literal);
+    }
+
+    fn number(&mut self) {
+        while Scanner::is_digit(self.peek()) {
+            self.advance();
+        }
+
+        if (self.peek() == Some('.') && Scanner::is_digit(self.peek_next())) {
+            self.advance();
+
+            while Scanner::is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let literal = Literal {
+            literal_type: LiteralType::Number,
+            value: LiteralValue {
+                number: self.source[self.start..self.current]
+                    .parse::<f64>()
+                    .unwrap(),
+            },
+        };
+
+        self.add_token_literal(TokenType::Number, literal);
+    }
+
+    fn is_digit(c: Option<char>) -> bool {
+        match c {
+            Some('0'..='9') => true,
+            _ => false,
+        }
+    }
+
+    fn is_alpha(c: Option<char>) -> bool {
+        match c {
+            Some('a'..='z') | Some('A'..='Z') | Some('_') => true,
+            _ => false,
+        }
+    }
+
+    fn is_alphanumeric(c: Option<char>) -> bool {
+        Scanner::is_digit(c) || Scanner::is_alpha(c)
+    }
+
+    fn indentifier(&mut self) {
+        while Scanner::is_alphanumeric(self.peek()) {
+            self.advance();
+        }
+
+        let text = &self.source[self.start..self.current];
+
+        let token_type = match Scanner::match_keyword(text) {
+            Some(token_type) => token_type,
+            None => TokenType::Identifier,
+        };
+
+        self.add_token(token_type);
+    }
+
+    fn match_keyword(text: &str) -> Option<TokenType> {
+        match text {
+            "and" => Some(TokenType::And),
+            "class" => Some(TokenType::Class),
+            "else" => Some(TokenType::Else),
+            "false" => Some(TokenType::False),
+            "fun" => Some(TokenType::Fun),
+            "for" => Some(TokenType::For),
+            "if" => Some(TokenType::If),
+            "nil" => Some(TokenType::Nil),
+            "or" => Some(TokenType::Or),
+            "print" => Some(TokenType::Print),
+            "return" => Some(TokenType::Return),
+            "super" => Some(TokenType::Super),
+            "this" => Some(TokenType::This),
+            "true" => Some(TokenType::True),
+            "var" => Some(TokenType::Var),
+            "while" => Some(TokenType::While),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -223,15 +407,14 @@ enum TokenType {
     Eof,
 }
 
-#[derive(Clone)]
-struct Token {
+struct Token<'a> {
     token_type: TokenType,
     lexeme: String,
-    literal: Option<Literal>,
+    literal: Option<Literal<'a>>,
     line: usize,
 }
 
-impl Token {
+impl Token<'_> {
     fn new(token_type: TokenType, lexeme: String, literal: Option<Literal>, line: usize) -> Token {
         return Token {
             token_type,
@@ -242,7 +425,7 @@ impl Token {
     }
 }
 
-impl Display for Token {
+impl Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         return match &self.literal {
             Some(l) => write!(f, "{:?} {} {}", self.token_type, self.lexeme, l),
@@ -251,13 +434,28 @@ impl Display for Token {
     }
 }
 
-#[derive(Clone, Copy)]
-union Literal {
-    number: usize,
+enum LiteralType {
+    String,
+    Number,
 }
 
-impl Display for Literal {
+struct Literal<'a> {
+    literal_type: LiteralType,
+    value: LiteralValue<'a>,
+}
+
+union LiteralValue<'a> {
+    number: f64,
+    string: &'a str,
+}
+
+impl Display for Literal<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unsafe { write!(f, "{}", self.number) }
+        unsafe {
+            match self.literal_type {
+                LiteralType::String => write!(f, "\"{}\"", self.value.string),
+                LiteralType::Number => write!(f, "{}", self.value.number),
+            }
+        }
     }
 }
